370738a1b509d324118b4b9702894cc08fd5e82d
anaorozcob <ana.orozco@correo.unimet.edu.ve>
2026-03-05 16:45:07 -0400

    Se agrega el modelo de procesos y estados del sistema
    Process state define los estados posibles de un proceso, locktype define los tipos de bloqueo y processcontrolBlock representa un proceso dentro del sistema con su información

---
+++ src/proyecto2so/kernel/ProcessControlBlock.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2so.kernel;
+
+/**
+ *
+ * @author ani
+ */
+
+import proyecto2so.core.Request;
+import proyecto2so.core.RequestOp;
+
+public class ProcessControlBlock {
+    private final int pid;
+    private final String user;
+
+    private ProcessState state;
+
+    private final Request request;
+
+    
+    private final String resourcePath;
+
+    private final LockType neededLock;
+
+    private String blockedReason;
+
+    public ProcessControlBlock(int pid, String user, Request request, String resourcePath) {
+        if (pid <= 0) throw new IllegalArgumentException("pid debe ser > 0");
+        if (user == null || user.trim().isEmpty()) throw new IllegalArgumentException("user inválido");
+        if (request == null) throw new IllegalArgumentException("request no puede ser null");
+        if (resourcePath == null || resourcePath.trim().isEmpty()) throw new IllegalArgumentException("resourcePath inválido");
+
+        this.pid = pid;
+        this.user = user;
+        this.request = request;
+        this.resourcePath = resourcePath;
+
+        this.neededLock = lockFromOp(request.getOp());
+        this.state = ProcessState.NEW;
+        this.blockedReason = null;
+    }
+
+    private LockType lockFromOp(RequestOp op) {
+        if (op == RequestOp.READ) return LockType.SHARED;
+        
+        return LockType.EXCLUSIVE;
+    }
+
+    public int getPid() { return pid; }
+    public String getUser() { return user; }
+
+    public ProcessState getState() { return state; }
+    public void setState(ProcessState state) { this.state = state; }
+
+    public Request getRequest() { return request; }
+
+    public String getResourcePath() { return resourcePath; }
+    public LockType getNeededLock() { return neededLock; }
+
+    public String getBlockedReason() { return blockedReason; }
+    public void setBlockedReason(String blockedReason) { this.blockedReason = blockedReason; }
+}
